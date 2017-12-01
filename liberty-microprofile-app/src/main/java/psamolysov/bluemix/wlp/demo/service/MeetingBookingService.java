149b44a93689484c2a2960f17203e09969deb9fe
Pavel Samolysov <samolisov@gmail.com>
2017-12-01 11:46:50 +0300

    Add the make method to the MeetingBookingService

--- liberty-microprofile-app/src/main/java/psamolysov/bluemix/wlp/demo/service/MeetingBookingService.java
+++ liberty-microprofile-app/src/main/java/psamolysov/bluemix/wlp/demo/service/MeetingBookingService.java
@@ -7,9 +7,12 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
+import java.util.UUID;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -32,5 +35,19 @@ public class MeetingBookingService {
 	@Produces(APPLICATION_JSON)
 	public Reservation get(@PathParam("id") String id) {		
 		return new Reservation(id, "Room 200", "100", "psamolysov", LocalDate.now(), LocalTime.now(), Duration.of(2, ChronoUnit.HOURS));
+	}
+	
+	@POST
+	@Path("/")
+	@Produces(APPLICATION_JSON)
+	@Consumes(APPLICATION_JSON)
+	public Reservation make(Reservation reservation) {
+		return new Reservation(UUID.randomUUID().toString(),
+				reservation.getVenue(),
+				reservation.getVenueId(),
+				reservation.getReservedBy(),
+				reservation.getDate(),
+				reservation.getStartTime(),
+				reservation.getDuration());
 	}
 }
